28458b77a8ab6a1961db36a700883ec37b2ed345
AlbaFroggy <182010716+AlbaFroggy@users.noreply.github.com>
2024-12-17 17:39:48 +0100

    Hover

--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -3,10 +3,11 @@ import React from "react";
 export const Button = ({ label = "Click", variant = "number", onClick }) => {
     // Definir clases base y variantes
     const baseStyles =
-        "transition duration-300 active:-translate-y-1 text-xl rounded-lg p-2";
+        "transition duration-300 active:-translate-y-1 text-xl rounded-lg p-2 hover:outline-none hover:ring-offset-2 hover:ring-2 ";
     const variantStyles = {
-        number: "bg-white text-purple-900",
-        operator: "bg-[#9e47aa] text-white",
+        number: "bg-white text-purple-900 hover:bg-blue-300 hover:ring-blue-300",
+        operator: "bg-[#9e47aa] text-white hover:bg-[#9e47aa]/80 hover:text-black hover:bg-purple-300 hover:ring-pink-300",
+        clear: "bg-blue-800 text-white rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl col-span-4 hover:text-black hover:bg-blue-100 hover:ring-blue-300",
     };
 
     return (
--- src/componentes/Calculator.jsx
+++ src/componentes/Calculator.jsx
@@ -1,15 +1,16 @@
+import logo from "../img/logo.jpg";
 import React, { useState } from "react";
+import { Button } from "./Boton";  // Importa el componente Button
 
 function Calculator() {
   const [result, setResult] = useState("");
 
-  const handleClick = (e) => setResult(result.concat(e.target.id));
+  const handleClick = (e) => setResult(result.concat(e.target.textContent));  // Usamos textContent
   const clear = () => setResult("");
   const deleteElement = () => setResult(result.slice(0, -1));
   const calculate = () => {
     try {
       let resultValue = eval(result);
-      // Redondear el resultado a 2 decimales
       setResult(parseFloat(resultValue.toFixed(8)).toString());
     } catch (error) {
       setResult("Invalid");
@@ -18,156 +19,127 @@ function Calculator() {
 
   return (
     <div className="bg-[#373147] border border-purple-600 w-80 mx-auto mt-20 p-4 rounded-lg shadow-lg">
+      <div className="flex items-center justify-center">
+        <img className="h-20 w-20 mb-5" src={logo} alt="Logo Azarquiel" />
+      </div>
       {/* Input */}
       <input
         type="text"
         value={result || "0"}
         disabled
-        className=" bg-white border border-purple-600 rounded-md w-full text-right py-1 px-5 text-2xl bg-transparent text-purple-900"
+        className="bg-white border border-purple-600 rounded-md w-full text-right py-1 px-5 text-2xl bg-transparent text-purple-900"
       />
 
       {/* Buttons */}
       <div className="grid grid-cols-4 gap-y-4 gap-x-3 mt-4">
         {/* Primera fila */}
-        <button
-          className="bg-[#2c2995] text-white rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl "
+        <Button
+          label="7"
+          variant="number"
+          onClick={handleClick}
+        />
+        <Button
+          label="8"
+          variant="number"
+          onClick={handleClick}
+        />
+        <Button
+          label="9"
+          variant="number"
+          onClick={handleClick}
+        />
+        <Button
+          label="+"
+          variant="operator"
+          onClick={handleClick}
+        />
+
+        {/* Segunda fila */}
+        <Button
+          label="4"
+          variant="number"
+          onClick={handleClick}
+        />
+        <Button
+          label="5"
+          variant="number"
+          onClick={handleClick}
+        />
+        <Button
+          label="6"
+          variant="number"
+          onClick={handleClick}
+        />
+        <Button
+          label="-"
+          variant="operator"
+          onClick={handleClick}
+        />
+
+        {/* Tercera fila */}
+        <Button
+          label="1"
+          variant="number"
+          onClick={handleClick}
+        />
+        <Button
+          label="2"
+          variant="number"
+          onClick={handleClick}
+        />
+        <Button
+          label="3"
+          variant="number"
+          onClick={handleClick}
+        />
+        <Button
+          label="*"
+          variant="operator"
+          onClick={handleClick}
+        />
+
+        {/* Cuarta fila */}
+        <Button
+          label="0"
+          variant="number"
+          onClick={handleClick}
+        />
+        <Button
+          label="."
+          variant="number"
+          onClick={handleClick}
+        />
+        <Button
+          label="/"
+          variant="operator"
+          onClick={handleClick}
+        />
+        <Button
+          label="="
+          variant="operator"
+          onClick={calculate}
+        />
+
+        {/* Clear */}
+        <Button
+          label="Clear"
+          variant="clear"
+          onClick={clear}
+          className="col-span-4"
+        />
+
+        {/* <button
+          className="bg-[#2c2995] text-white rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl col-span-2 "
           onClick={clear}
         >
           AC
         </button>
         <button
-          className="bg-[#2c2995] text-white rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
+          className="bg-[#2c2995] text-white rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl col-span-2"
           onClick={deleteElement}
         >
           DE
-        </button>
-        <button
-          id="."
-          className="bg-[#9e47aa] text-white rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          .
-        </button>
-        <button
-          id="/"
-          className="bg-[#9e47aa] text-white rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          /
-        </button>
-
-        {/* Segunda fila */}
-        <button
-          id="7"
-          className="bg-white text-purple-900 rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          7
-        </button>
-        <button
-          id="8"
-          className="bg-white text-purple-900 rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          8
-        </button>
-        <button
-          id="9"
-          className="bg-white text-purple-900 rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          9
-        </button>
-        <button
-          id="*"
-          className="bg-[#9e47aa] text-white rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          *
-        </button>
-
-        {/* Tercera fila */}
-        <button
-          id="4"
-          className="bg-white text-purple-900 rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          4
-        </button>
-        <button
-          id="5"
-          className="bg-white text-purple-900 rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          5
-        </button>
-        <button
-          id="6"
-          className="bg-white text-purple-900 rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          6
-        </button>
-        <button
-          id="-"
-          className="bg-[#9e47aa] text-white rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          -
-        </button>
-
-        {/* Cuarta fila */}
-        <button
-          id="1"
-          className="bg-white text-purple-900 rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          1
-        </button>
-        <button
-          id="2"
-          className="bg-white text-purple-900 rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          2
-        </button>
-        <button
-          id="3"
-          className="bg-white text-purple-900 rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          3
-        </button>
-        <button
-          id="+"
-          className="bg-[#9e47aa] text-white rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          +
-        </button>
-
-        {/* Última fila */}
-        <button
-          id="00"
-          className="bg-white text-purple-900 rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          00
-        </button>
-        <button
-          id="0"
-          className="bg-white text-purple-900 rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl"
-          onClick={handleClick}
-        >
-          0
-        </button>
-        <button
-          id="="
-          className="bg-[#9e47aa] text-white rounded-lg p-2 transition duration-300 active:-translate-y-1 text-xl col-span-2"
-          onClick={calculate}
-        >
-          =
-        </button>
+        </button> */}
       </div>
     </div>
   );
